e1a3ecdb1da3b51af0af3ec97f5cd74d88a364d0
Kamil Skorzynski <skorzynskikamil@gmail.com>
2020-06-05 13:13:16 +0200

    config

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  db: '',
+  db:
+    'mongodb+srv://admin-123:25ExMwEUJhM-9s_@cluster0-j2qwk.mongodb.net/test?retryWrites=true&w=majority',
   keySession: ['TWÓJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000,
 };
